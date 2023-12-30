3afd033a04fbb4392da5cfd0fcba4fd4c203b074
hamza-aloglu <hamza-aloglu@hotmail.com>
2023-12-30 14:01:07 +0300

    implement GenerateIndividual for task scheduling example

--- main.go
+++ main.go
@@ -1,71 +1,101 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"github.com/hamza-aloglu/GeneticAlgo-Go/src"
 	"math/rand"
 	"os"
 	"os/exec"
 	"runtime"
-	"strings"
 	"time"
 )
 
-var (
-	alleles []string = strings.Split("abcdefghijklmnopqrstuvwxyz,! ", "")
-	target  []string = strings.Split("hello, world!", "")
-)
+// Schedule -> days -> tasks
 
-// Characters represent genes and implements Individual
-type Characters []string
+type Task struct {
+	Title      string
+	Deadline   time.Time
+	Difficulty int
+	Priority   int
+}
 
-func (c Characters) CalculateFitness() float64 {
-	result := 0.0
-	for i, gene := range c {
-		if gene == target[i] {
-			result += 1.0
+type Day struct {
+	tasks []Task
+}
+
+type Schedule []Day
+
+var dayAmount = 5
+var insertChance = 0.2
+
+// Creating dummy data for Task
+var task1 = Task{
+	Title:      "Implement User Authentication",
+	Deadline:   time.Now().AddDate(0, 0, 7), // Deadline is set to 7 days from now
+	Difficulty: 3,
+	Priority:   2,
+}
+
+var task2 = Task{
+	Title:      "Refactor Database Layer",
+	Deadline:   time.Now().AddDate(0, 0, 14), // Deadline is set to 14 days from now
+	Difficulty: 4,
+	Priority:   1,
+}
+
+var task3 = Task{
+	Title:      "Update UI Components",
+	Deadline:   time.Now().AddDate(0, 0, 5), // Deadline is set to 5 days from now
+	Difficulty: 2,
+	Priority:   3,
+}
+
+var tasks = []Task{task1, task2, task3}
+
+func (s Schedule) CalculateFitness() float64 {
+	panic("implement calcualte fitness")
+}
+
+func (s Schedule) GenerateIndividual() src.Individual {
+	var localTasks []Task
+	localTasks = append(localTasks, tasks...)
+	individual := make(Schedule, dayAmount)
+	for len(localTasks) > 0 {
+		for i := 0; i < dayAmount && len(localTasks) > 0; i++ {
+			if rand.Float64() < insertChance {
+				// insert randomly selected task to the day
+				randomTaskIndex := rand.Intn(len(localTasks))
+				individual[i].tasks = append(individual[i].tasks, localTasks[randomTaskIndex])
+
+				// remove randomly selected task from whole tasks
+				localTasks = append(localTasks[:randomTaskIndex], localTasks[randomTaskIndex+1:]...)
+			}
 		}
 	}
-
-	return result
-}
-
-func (c Characters) GenerateIndividual() src.Individual {
-	individual := make(Characters, len(target))
-	for i := 0; i < len(target); i++ {
-		individual[i] = alleles[rand.Intn(len(alleles))]
-	}
 	return individual
-}
-
-type StringModel struct {
-	src.DefaultModel
-}
-
-func (sm StringModel) Mutate(individual src.Individual) (src.Individual, error) {
-	// Assert the individual to type Characters
-	charactersIndividual, ok := individual.(Characters)
-	if !ok {
-		// Handle the case where the assertion fails, e.g., return an error
-		return nil, errors.New("individual is not of type Characters")
-	}
-	// Perform the mutation on charactersIndividual
-	charactersIndividual[rand.Intn(len(charactersIndividual))] = alleles[rand.Intn(len(alleles))]
-	return charactersIndividual, nil
 }
 
 func main() {
 	defer timer("main")()
 
-	// Models can be given using Enum. (Models.StringModel).
-	ga := src.NewCustomGA(40, 2500, 0.5, Characters{}, StringModel{})
-	var bestCharacters Characters
-	bestCharacters = ga.Run().(Characters)
-	for _, gen := range bestCharacters {
+	ga := src.NewDefaultGA(40, 2500, 0.5, Schedule{})
+	var bestSchedule Schedule
+	bestSchedule = ga.Run().(Schedule)
+	for _, gen := range bestSchedule {
 		fmt.Printf("\n%v", gen)
 	}
 
+	printBenchmark()
+}
+
+func timer(name string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", name, time.Since(start))
+	}
+}
+
+func printBenchmark() {
 	// Collect and print memory usage
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
@@ -80,12 +110,5 @@ func main() {
 	err := cmd.Run()
 	if err != nil {
 		fmt.Println("Error getting CPU usage:", err)
-	}
-}
-
-func timer(name string) func() {
-	start := time.Now()
-	return func() {
-		fmt.Printf("%s took %v\n", name, time.Since(start))
 	}
 }
